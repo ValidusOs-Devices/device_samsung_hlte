eec826686be79934d49e76296ee3feabd4531fdc
allianceSparx <alliancesparx@gmail.com>
2018-02-10 23:06:10 +0000

    Lineage --> Validus

--- validus.mk
+++ validus.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2013-2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2018 Ground Zero Roms
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
+
+PRODUCT_NAME := validus_hlte
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := lineage_hlte
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SM-N9005
